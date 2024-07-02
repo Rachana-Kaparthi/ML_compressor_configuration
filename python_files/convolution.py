fac66431ea1afdee57998483df50d50a152d7662
Rachana.Kaparthi <rachana.kaparthi@iiitb.ac.in>
2024-07-03 00:33:11 +0530

    multiprocessing and dictionary

--- python_files/convolution.py
+++ python_files/convolution.py
@@ -43,7 +43,7 @@ def conv_3x3_sobel_approx(ipimage,kernal1,pos):
     # print(pos)
     # pos1=[[pos[0],pos[1],pos[2]],[pos[3],pos[4],pos[5]],[pos[6],pos[7],pos[8]]]
     # pos2=[[pos[9],pos[10],pos[11]],[pos[12],pos[13],pos[14]],[pos[15],pos[16],pos[17]]]
-  
+    dict = {}
   
     height=len(ipimage)
     width=len(ipimage[0])
@@ -58,10 +58,11 @@ def conv_3x3_sobel_approx(ipimage,kernal1,pos):
     for i in range(1,height-1):
         for j in range(1,width-1):
             inmat=[ipimage[i+1][j-1:j+2], ipimage[i][j-1:j+2]  ,ipimage[i-1][j-1:j+2]]
-            if(inmat == [[0,0,0],[0,0,0],[0,0,0]]):
-                approx_op_k1[i][j] = 0
-            else :
-                approx_op_k1[i][j] = matrix_mul_approx(inmat,kernal1,pos)
+            inmat_tuple = tuple(tuple(row) for row in inmat)
+            if inmat_tuple not in dict:
+                dict[inmat_tuple] = matrix_mul_approx(inmat,kernal1,pos)
+            
+            approx_op_k1[i][j] = dict[inmat_tuple]
             
             # approx_op_k1[i][j]=(approxop_k1[0][0]+approxop_k1[1][1]+approxop_k1[2][2])/32
 
--- python_files/main.py
+++ python_files/main.py
@@ -6,94 +6,116 @@ import itertools
 import openpyxl
 from openpyxl import Workbook
 import csv
+import time
+import multiprocessing
+from functools import partial
 
 
+def util(l, i, j, k, inputimage, kernal, exactop, count):
+    start_time = time.time()
 
-inputimage = imread('cameraman.png')
-inputimage = (inputimage/2).astype(int)
+    # print(f"i = {i}, j = {j}, k = {k}, l = {l}")
+    pos = [i, j, k, l]
+    # pos = [1,4,5,6]
+    print(pos)
+    [approxop]=conv_3x3_sobel_approx(inputimage,kernal,pos)
+    ssim = metrics.structural_similarity(exactop, approxop, data_range=1.0)
+    mul = "mul" + str(count)
 
-# kernal1 = np.array([[-32, 0, 32],[-64,  0, 64],[-32, 0, 32]])
-# kernal2 = np.array([[-32, -64, -32],[0,  0, 0],[32, 64, 32]])
-
-kernal = np.array([[-127,-128,-89],[-31,10,52],[51,101,127]])
-
-
-[exactop]=conv_3x3_sobel_exact(inputimage,kernal)
-print("exacttop done")
-
-# Create a workbook and select the active sheet
-wb = Workbook()
-ws = wb.active
-
-# Write headers
-ws['A1'] = 'Multiplier'
-ws['B1'] = 'Position'
-ws['C1'] = 'SSIM'
-ws['D1'] = 'Area'
-ws['E1'] = 'Power'
-ws['F1'] = 'Cost function'
-
-# Open a CSV file and create a CSV writer
-csv_file = open('output.csv', mode='w', newline='')
-csv_writer = csv.writer(csv_file)
-
-# Write headers to CSV
-csv_writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
+    # Hardware parameters extrapolation
+    area=area_45nm(pos)
+    power=power_45nm(pos)
 
 
-# Generate all combinations of the ranges
-# combinations = itertools.product(range(1, 5), repeat=4)
+    # # Write data to the Excel file
+    # ws.cell(row=row_counter, column=1, value=mul)  # Multiplier
+    # ws.cell(row=row_counter, column=2, value=str(pos))  # Position
+    # ws.cell(row=row_counter, column=3, value=ssim)  # SSIM
+    # ws.cell(row=row_counter, column=4, value=area)  # Area
+    # ws.cell(row=row_counter, column=5, value=power)  # Power
+    # ws.cell(row=row_counter, column=6, value=area * power)  # Cost function
 
-# Initialize row counter starting from the second row
-row_counter = 2
+    # Write data to the CSV file
+    with open('output.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([mul, pos, ssim, area, power, area * power])
 
-# # Create lists to store the combinations, SSIM values, and multipliers
-# data = []
-# SSIM = []
-# multiplier = []
-# AreaPowerProd = []
-# Initialize count
-count = 1
-
-
-# Print each combination
-for i in range(1,10):
-    for j in range(1,10):
-        for k in range(1,10):
-            for l in range(1,10):
-    
-                # print(f"i = {i}, j = {j}, k = {k}, l = {l}")
-                pos = [i, j, k, l]
-                # pos = [1,4,5,6]
-                print(pos)
-                [approxop]=conv_3x3_sobel_approx(inputimage,kernal,pos)
-                ssim = metrics.structural_similarity(exactop, approxop, data_range=1.0)
-                mul = "mul" + str(count)
-
-                # Hardware parameters extrapolation
-                area=area_45nm(pos)
-                power=power_45nm(pos)
+    # row_counter += 1
+    count += 1    
+    print("            SSIM : ",ssim)
+    print("            Cost Function :", area*power)
+    print("Time taken ", time.time() - start_time)
 
 
-                # Write data to the Excel file
-                ws.cell(row=row_counter, column=1, value=mul)  # Multiplier
-                ws.cell(row=row_counter, column=2, value=str(pos))  # Position
-                ws.cell(row=row_counter, column=3, value=ssim)  # SSIM
-                ws.cell(row=row_counter, column=4, value=area)  # Area
-                ws.cell(row=row_counter, column=5, value=power)  # Power
-                ws.cell(row=row_counter, column=6, value=area * power)  # Cost function
+if __name__ == "__main__":
+    inputimage = imread('./ML_compressor_configuration/python_files/cameraman.png')
+    inputimage = (inputimage/2).astype(int)
 
-                # Write data to the CSV file
-                csv_writer.writerow([mul, pos, ssim, area, power, area * power])
+    # kernal1 = np.array([[-32, 0, 32],[-64,  0, 64],[-32, 0, 32]])
+    # kernal2 = np.array([[-32, -64, -32],[0,  0, 0],[32, 64, 32]])
 
-                row_counter += 1
-                count += 1    
-                print("            SSIM : ",ssim)
-                print("            Cost Function :", area*power)
-
-# Save the workbook
-wb.save('combinations.xlsx')
+    kernal = np.array([[-127,-128,-89],[-31,10,52],[51,101,127]])
 
 
-# Close the CSV file
-csv_file.close()
+    [exactop]=conv_3x3_sobel_exact(inputimage,kernal)
+    print("exacttop done")
+
+    # Create a workbook and select the active sheet
+    wb = Workbook()
+    ws = wb.active
+
+    # Write headers
+    ws['A1'] = 'Multiplier'
+    ws['B1'] = 'Position'
+    ws['C1'] = 'SSIM'
+    ws['D1'] = 'Area'
+    ws['E1'] = 'Power'
+    ws['F1'] = 'Cost function'
+
+    # # Open a CSV file and create a CSV writer
+    # csv_file = open('output.csv', mode='w', newline='')
+    # csv_writer = csv.writer(csv_file)
+
+    # # Write headers to CSV
+    # csv_writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
+    with open('output.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
+
+
+    # Generate all combinations of the ranges
+    # combinations = itertools.product(range(1, 5), repeat=4)
+
+    # Initialize row counter starting from the second row
+    # row_counter = 2
+
+    # # Create lists to store the combinations, SSIM values, and multipliers
+    # data = []
+    # SSIM = []
+    # multiplier = []
+    # AreaPowerProd = []
+    # Initialize count
+    count = 1
+
+
+    # Print each combination
+    for i in range(1,2):
+        for j in range(1,2):
+            for k in range(1,2):
+                st = time.time()
+                numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+                pool = multiprocessing.Pool(processes=8)
+                partial_func = partial(util, i=i, j=j, k=k, inputimage=inputimage, kernal=kernal, exactop=exactop, count=count)
+                pool.map(partial_func, numbers)
+                pool.close()
+                pool.join()
+                print("Total Time taken ", time.time() - st)
+                    
+
+    # Save the workbook
+    # wb.save('combinations.xlsx')
+
+
+    # Close the CSV file
+    # csv_file.close()
+
