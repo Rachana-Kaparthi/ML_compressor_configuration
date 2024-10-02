95466d180344479801276aff7a39ef0350635ed5
Rachana Kaparthi <140998470+Rachana-Kaparthi@users.noreply.github.com>
2024-10-02 10:56:39 +0530

    modified multiprocess_main.py for mnist dataset

--- python_files/multiprocess_main.py
+++ python_files/multiprocess_main.py
@@ -3,13 +3,13 @@ from convolution import *
 from area_power import *
 
 import csv
-import time
+# import time
 import multiprocessing
 from functools import partial
 
 
 def util(l, i, j, k, inputimage, kernal, exactop, opfile):
-    start_time = time.time()
+    # start_time = time.time()
 
     # print(f"i = {i}, j = {j}, k = {k}, l = {l}")
     pos = [i, j, k, l]
@@ -27,11 +27,13 @@ def util(l, i, j, k, inputimage, kernal, exactop, opfile):
     # Write data to the CSV file
     with open(opfile, mode='a', newline='') as file:
         writer = csv.writer(file)
+        # writer.writerow([mul, pos, ssim])
         writer.writerow([mul, pos, ssim, area, power, area * power])
    
-    print("            SSIM : ",ssim)
-    print("            Cost Function :", area*power)
-    print("Time taken ", time.time() - start_time)
+   
+    # print("            SSIM : ",ssim)
+    # print("            Cost Function :", area*power)
+    # print("Time taken ", time.time() - start_time)
 
 
 if __name__ == "__main__":
@@ -41,39 +43,41 @@ if __name__ == "__main__":
 # kernal=[[-0.0215, -0.0401,  0.0427];
 #          [ 0.0514, -0.0458, -0.0348];
 #          [-0.0442,  0.0148, -0.0608]];
+    for digit in range(0,10): 
+        for count in range(0,100):
+            ipfilename = f"./mnist_images/{digit}/{digit}_"+str(count)+".png"
+            inputimage = imread(ipfilename)
+            inputimage = (inputimage/2).astype(int)
 
-    for count in range(1,101):
-        ipfilename = "./fashion_mnist_images/Trouser/Trouser_"+str(count)+".png"
-        inputimage = imread(ipfilename)
-        inputimage = (inputimage/2).astype(int)
+            [exactop]=conv_3x3_sobel_exact(inputimage,kernal)
+            print(f"exacttop done for {ipfilename}")
+            opfile = f"./dataset_mnist/{digit}/{digit}_"+str(count)+".csv"
+        
 
-        [exactop]=conv_3x3_sobel_exact(inputimage,kernal)
-        print(f"exacttop done for {ipfilename}")
-        opfile = "./dataset/Trouser/Trouser_"+str(count)+".csv"
-    
+            # # Open a CSV file and create a CSV writer
+            # csv_file = open('output.csv', mode='w', newline='')
+            # csv_writer = csv.writer(csv_file)
 
-        # # Open a CSV file and create a CSV writer
-        # csv_file = open('output.csv', mode='w', newline='')
-        # csv_writer = csv.writer(csv_file)
-
-        # # Write headers to CSV
-        # csv_writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
-        with open(opfile, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
+            # # Write headers to CSV
+            # csv_writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
+            with open(opfile, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                # writer.writerow(['Multiplier', 'Position', 'SSIM'])
+                writer.writerow(['Multiplier', 'Position', 'SSIM', 'Area', 'Power', 'Cost function'])
 
 
-        st = time.time()
-        # Print each combination
-        for i in range(1,10):
-            for j in range(1,10):
-                for k in range(1,10):
-                
-                    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                    pool = multiprocessing.Pool(processes=9)
-                    partial_func = partial(util, i=i, j=j, k=k, inputimage=inputimage, kernal=kernal, exactop=exactop, opfile=opfile)
-                    pool.map(partial_func, numbers)
-                    pool.close()
-                    pool.join()
-        print("Total Time taken ", time.time() - st)
-                        
+
+            # st = time.time()
+            # Print each combination
+            for i in range(1,10):
+                for j in range(1,10):
+                    for k in range(1,10):
+                    
+                        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                        pool = multiprocessing.Pool(processes=9)
+                        partial_func = partial(util, i=i, j=j, k=k, inputimage=inputimage, kernal=kernal, exactop=exactop, opfile=opfile)
+                        pool.map(partial_func, numbers)
+                        pool.close()
+                        pool.join()
+            # print("Total Time taken ", time.time() - st)
+                            
